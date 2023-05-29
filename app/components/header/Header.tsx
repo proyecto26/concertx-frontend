0da527848dd55670f264c26bf109c048b74d303b
Juan David Nicholls <jdnichollsc@hotmail.com>
2023-05-28 23:09:36 -0500

    fix header shadow for dark theme

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -40,7 +40,7 @@ const Header: React.FC<HeaderProps> = ({ title = 'ConcertX' }) => {
       <header
         ref={headerRef}
         className={clsx(
-          'sticky top-0 z-50 flex flex-wrap items-center justify-between bg-primary-contrast px-4 py-1 shadow-md shadow-slate-900/5 transition duration-500 dark:shadow-none sm:px-6 lg:px-8',
+          'sticky top-0 z-50 flex flex-wrap items-center justify-between bg-primary-contrast px-4 py-1 shadow-md dark:shadow-2xl shadow-slate-900/5 transition duration-500 sm:px-6 lg:px-8',
           isScrolled
             ? 'dark:bg-slate-900/95 dark:backdrop-blur dark:[@supports(backdrop-filter:blur(0))]:bg-slate-900/75'
             : 'dark:bg-transparent'
